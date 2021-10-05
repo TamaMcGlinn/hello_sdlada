909d2ca9c39ae1bdfe929bd98c57525c3a3cb1a4
Tama McGlinn <t.mcglinn@gmail.com>
2021-10-05 21:42:02 +0200

    Modified example to be simple color tween

--- src/hello_sdl.adb
+++ src/hello_sdl.adb
@@ -4,6 +4,7 @@ with SDL;
 with SDL.Timers;
 with SDL.Video.Pixel_Formats;
 with SDL.Video.Surfaces;
+with SDL.Video.Palettes;
 with SDL.Video.Windows;
 with SDL.Video.Windows.Makers;
 
@@ -14,47 +15,41 @@ procedure Hello_SDL is
      US.To_Unbounded_String;
    function "+" (S : US.Unbounded_String) return String renames US.To_String;
 
-   type Testcase is record
-      Label    : US.Unbounded_String;
-      Position : SDL.Natural_Coordinates;
-   end record;
-
-   Tests : constant array (Positive range <>) of Testcase :=
-     ((+"X: Undefined position (0)",
-       (SDL.Video.Windows.Undefined_Window_Position (0), 100)),
-      (+"Y: Undefined position (1)",
-       (100, SDL.Video.Windows.Undefined_Window_Position (1))),
-      (+"Y: Centered position (0)",
-       (100, SDL.Video.Windows.Centered_Window_Position (0))),
-      (+"X: Centered position (1)",
-       (SDL.Video.Windows.Centered_Window_Position (1), 100)));
+   Label    : constant US.Unbounded_String     := +"Hello SDL";
+   Position : constant SDL.Natural_Coordinates :=
+     (SDL.Video.Windows.Undefined_Window_Position (0), 100);
+   Width  : constant := 400;
+   Height : constant := 300;
 
 begin
    if not SDL.Initialise then
       raise Program_Error;
    end if;
 
-   for T of Tests loop
-      declare
-         Width  : constant := 400;
-         Height : constant := 300;
-         W      : SDL.Video.Windows.Window;
-         S      : SDL.Video.Surfaces.Surface;
-      begin
-         SDL.Video.Windows.Makers.Create
-           (W, +T.Label, T.Position, (Width, Height));
-         S := W.Get_Surface;
-         for I in 1 .. 10 loop
+   declare
+      W : SDL.Video.Windows.Window;
+      S : SDL.Video.Surfaces.Surface;
+   begin
+      SDL.Video.Windows.Makers.Create (W, +Label, Position, (Width, Height));
+      S := W.Get_Surface;
+      for I in 1 .. 255 loop
+         declare
+            R : constant SDL.Video.Palettes.Colour_Component :=
+              SDL.Video.Palettes.Colour_Component (I);
+            G : constant SDL.Video.Palettes.Colour_Component :=
+              SDL.Video.Palettes.Colour_Component (255 - I);
+            B : constant SDL.Video.Palettes.Colour_Component :=
+              SDL.Video.Palettes.Colour_Component (128 + (I / 2));
+         begin
             S.Fill
               (Area   => (0, 0, Width, Height),
                Colour =>
-                 SDL.Video.Pixel_Formats.To_Pixel
-                   (S.Pixel_Format, 255, 255, 255));
+                 SDL.Video.Pixel_Formats.To_Pixel (S.Pixel_Format, R, G, B));
             W.Update_Surface;
-            SDL.Timers.Wait_Delay (100);
-         end loop;
-      end;
-   end loop;
+            SDL.Timers.Wait_Delay (10);
+         end;
+      end loop;
+   end;
 
    SDL.Finalise;
 end Hello_SDL;
