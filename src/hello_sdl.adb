6a22515a37a2b04de60adc33c112d1c742705279
Tama McGlinn <t.mcglinn@gmail.com>
2021-10-06 21:13:47 +0200

    Add main event loop with keyboard handling

--- src/hello_sdl.adb
+++ src/hello_sdl.adb
@@ -1,10 +1,16 @@
+with Ada.Text_IO;
+
 with SDL;
+with SDL.Events;
+with SDL.Events.Events;
+with SDL.Events.Keyboards;
 with SDL.Timers;
-with SDL.Video.Pixel_Formats;
-with SDL.Video.Surfaces;
-with SDL.Video.Palettes;
+with SDL.Video.Renderers;
+with SDL.Video.Renderers.Makers;
 with SDL.Video.Windows;
 with SDL.Video.Windows.Makers;
+
+with SDL.Images;
 
 procedure Hello_SDL is
 
@@ -14,35 +20,67 @@ procedure Hello_SDL is
    Width  : constant := 400;
    Height : constant := 300;
 
-begin
-   if not SDL.Initialise then
-      raise Program_Error;
-   end if;
+   Window   : SDL.Video.Windows.Window;
+   Renderer : SDL.Video.Renderers.Renderer;
 
-   declare
-      W : SDL.Video.Windows.Window;
-      S : SDL.Video.Surfaces.Surface;
+   procedure Init_SDL is
    begin
-      SDL.Video.Windows.Makers.Create (W, Label, Position, (Width, Height));
-      S := W.Get_Surface;
-      for I in 1 .. 255 loop
-         declare
-            R : constant SDL.Video.Palettes.Colour_Component :=
-              SDL.Video.Palettes.Colour_Component (I);
-            G : constant SDL.Video.Palettes.Colour_Component :=
-              SDL.Video.Palettes.Colour_Component (255 - I);
-            B : constant SDL.Video.Palettes.Colour_Component :=
-              SDL.Video.Palettes.Colour_Component (128 + (I / 2));
-         begin
-            S.Fill
-              (Area   => (0, 0, Width, Height),
-               Colour =>
-                 SDL.Video.Pixel_Formats.To_Pixel (S.Pixel_Format, R, G, B));
-            W.Update_Surface;
-            SDL.Timers.Wait_Delay (10);
-         end;
-      end loop;
-   end;
+      if not SDL.Initialise then
+         raise Program_Error;
+      end if;
+      if not SDL.Images.Initialise then
+         raise Program_Error;
+      end if;
+      SDL.Video.Windows.Makers.Create
+        (Window, Label, Position, (Width, Height));
+      SDL.Video.Renderers.Makers.Create (Renderer, Window);
+   end Init_SDL;
 
-   SDL.Finalise;
+   procedure Shutdown_SDL is
+   begin
+      SDL.Images.Finalise;
+      SDL.Finalise;
+   end Shutdown_SDL;
+
+   procedure Update_Display is
+   begin
+      null;
+   end Update_Display;
+
+   procedure Main_Loop is
+      Event : SDL.Events.Events.Events;
+   begin
+      loop
+         if SDL.Events.Events.Poll (Event) then
+            case Event.Common.Event_Type is
+               when SDL.Events.Keyboards.Key_Down =>
+                  declare
+                     use type SDL.Events.Keyboards.Key_Codes;
+                     use SDL.Events.Keyboards;
+                     Key : constant Key_Codes :=
+                       Event.Keyboard.Key_Sym.Key_Code;
+                  begin
+                     case Key is
+                        when Code_Escape =>
+                           return;
+                        when Code_0 =>
+                           Ada.Text_IO.Put_Line ("Pressed 0");
+                        when others =>
+                           Ada.Text_IO.Put_Line ("Pressed something");
+                     end case;
+                  end;
+               when SDL.Events.Quit =>
+                  return;
+               when others =>
+                  null;
+            end case;
+         end if;
+         Update_Display;
+      end loop;
+   end Main_Loop;
+
+begin
+   Init_SDL;
+   Main_Loop;
+   Shutdown_SDL;
 end Hello_SDL;
